1a8235fdf6a6f47ab3f5cf09c5a59975ff5d736b
Denis Bozhenkov <mm@karellens.com>
2018-09-13 18:13:17 +0300

    count combined fields. fix parse invalid strings.

--- per_day_counter.go
+++ per_day_counter.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha1"
+	"encoding/base32"
 	"encoding/json"
 	"flag"
 	"io"
@@ -11,7 +13,17 @@ import (
 	"time"
 )
 
-func retrieveMatches(line string, regExp *regexp.Regexp) map[string]string {
+func retrieveMatches(line string, regExp *regexp.Regexp) (map[string]string, error) {
+	var err error
+	err = nil
+	defer func() {
+		e := recover()
+		if panicErr, ok := e.(error); ok {
+			panicErr = panicErr
+			return
+		}
+	}()
+
 	match := regExp.FindStringSubmatch(line)
 	matches := make(map[string]string)
 	for i, name := range regExp.SubexpNames() {
@@ -20,25 +32,26 @@ func retrieveMatches(line string, regExp *regexp.Regexp) map[string]string {
 		}
 	}
 
-	return matches
+	return matches, err
 }
 
 func main() {
+	// Available for retrieve: count_*, ip, date, datetime, method, uri, query, statuscode, bytessent, refferer, useragent.
+	// You can combine the above fields with `+` to calculate the number of unique entries
 	sourceFilePtr := flag.String("source", "nginx-access.log", "source nginx log file")
 	destFilePtr := flag.String("destination", "urls.json", "destination json results file contains grouped top locations")
-	//groupBy := flag.String("group-by", "", "field to group by")
-	//available for retrieve: count_*, visitors (ip+useragent), ip, date, datetime, method, uri, query, statuscode, bytessent, refferer, useragent
+	//pretty := flag.Bool("pretty", False, "Pretty JSON output")
 	retrieve := flag.String("get", "", "what fields to retrieve")
 	flag.Parse()
 	retrieveFields := strings.Split(*retrieve, " ")
 
 	file, _ := ioutil.ReadFile(*sourceFilePtr)
 	buf := bytes.NewBuffer(file)
-	// todo split datetime on date and time
+	// TODO: split datetime on date and time
 	sourceLinePattern := `(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(?P<datetime>\d{2}\/[A-Za-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] (("(?P<method>GET|POST|HEAD) )(?P<uri>.+?)(?P<query>\?.*)? (HTTP\/\d\.\d")) (?P<statuscode>\d{3}) (?P<bytessent>\d+) (["](?P<refferer>(\-)|(.*))["]) (["](?P<useragent>.+)["])`
 	re := regexp.MustCompile(sourceLinePattern)
 
-	// except insignificant requests
+	// except insignificant requests. You can add your own patterns
 	insignificantOccurrences := `\.woff|\.ttf|\.eot|\.svg|.ico|\.png|\.jpg|\.jpeg|\.gif|\.mp4|\.css\.map|\.js\.map|\.js|\.css|get\-file\?id|robots\.txt|\/admin`
 	reInsOcc := regexp.MustCompile(insignificantOccurrences)
 
@@ -52,7 +65,10 @@ func main() {
 			}
 		}
 
-		matches := retrieveMatches(line, re)
+		matches, err := retrieveMatches(line, re)
+		if err != nil {
+			continue
+		}
 
 		// match only GET except insignificantOccurrences
 		if matches["method"] == "GET" && !reInsOcc.MatchString(matches["uri"]) {
@@ -61,24 +77,42 @@ func main() {
 			date_str = date.Format("2006-01-02")
 
 			for _, field := range retrieveFields {
+				var field_value string
+
+				if strings.Contains(field, "+") {
+					combined_fields := strings.Split(field, "+")
+					var unpacked_field_value string
+
+					for _, cf := range combined_fields {
+						unpacked_field_value += matches[cf]
+					}
+					hash := sha1.Sum([]byte(unpacked_field_value))
+					field_value = base32.HexEncoding.EncodeToString(hash[:])
+
+				} else {
+					field_value = matches[field]
+				}
+
 				if _, ok := container[date_str]; ok {
 					if _, ok2 := container[date_str][field]; ok2 {
-						container[date_str][field][matches[field]]++
+						container[date_str][field][field_value]++
 					} else {
 						temp := make(map[string]int)
-						temp[matches[field]]++
+						temp[field_value]++
 						container[date_str][field] = temp
 					}
 				} else {
 					temp := make(map[string]map[string]int)
 					temp[field] = make(map[string]int)
-					temp[field][matches[field]]++
+					temp[field][field_value]++
 					container[date_str] = temp
 				}
 			}
 		}
 	}
+	// TODO: add ability to count unique entries
 
+	// TODO: pretty
 	//output, _ := json.Marshal(container)
 	output, _ := json.MarshalIndent(container, "", "    ")
 
